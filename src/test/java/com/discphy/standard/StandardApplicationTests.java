aa78e12ac907086d52b852d7038f879972baaf34
discphy <dinophy@nate.com>
2024-06-20 17:34:30 +0900

    feat(add): Initial project

---
+++ src/test/java/com/discphy/standard/StandardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discphy.standard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StandardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
